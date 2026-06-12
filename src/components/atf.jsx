88067362c6ae00cd915fbc5ab9a732faf9146cc3
Ballast <163994830+Ballastt@users.noreply.github.com>
2026-06-12 17:40:05 +0200

    img path fixed

--- src/components/atf.jsx
+++ src/components/atf.jsx
@@ -1,7 +1,7 @@
 export default function Atf() {
   return (
     <header className="atf">
-      <img src="./public/hero-self.jpg" alt="Picture of BL" width="100" className="atf--image" />
+      <img src="/hero-self.jpg" alt="Picture of BL" width="100" className="atf--image" />
     </header>
   )
 }
